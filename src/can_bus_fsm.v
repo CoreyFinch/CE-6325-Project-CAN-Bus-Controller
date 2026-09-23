df6dd747caef944884314e35148469f06b075716
CoreyFinch <coreyfinch2@gmail.com>
2026-09-23 10:02:15 -0500

    Updated for synthesis

--- src/can_bus_fsm.v
+++ src/can_bus_fsm.v
@@ -7,9 +7,17 @@
     - error handling
     
 */
-`include "../src/Params.v"
 
-module CAN_BUS (
+module CAN_BUS #(
+    parameter ID_WIDTH = 11,
+    parameter DLC_WIDTH = 4,
+    parameter DATA_WIDTH = 8*8, // 8 bytes * 8 bits/byte
+    parameter CRC_WIDTH = 15,
+    parameter EOF_WIDTH = 7,
+    parameter NUM_STATES_WIDTH = 4,
+    parameter BIT_CNT_WIDTH = 7,
+    parameter IFS_WIDTH = 3
+)(
     /* Inputs */
     input clk,
     input reset,
--- testbench/can_tb.v
+++ testbench/can_tb.v
@@ -15,9 +15,16 @@
         reset   = 1'b1 to reset the FSM
 */
 
-`include "../src/Params.v"
-
-module CAN_BUS_TB ();
+module CAN_BUS_TB #(
+    parameter ID_WIDTH = 11,
+    parameter DLC_WIDTH = 4,
+    parameter DATA_WIDTH = 8*8, // 8 bytes * 8 bits/byte
+    parameter CRC_WIDTH = 15,
+    parameter EOF_WIDTH = 7,
+    parameter NUM_STATES_WIDTH = 4,
+    parameter BIT_CNT_WIDTH = 7,
+    parameter IFS_WIDTH = 3
+)();
     /* Inputs */
     reg clk;                     
     reg reset;                   
